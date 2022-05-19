7a3a8a9f326a38df307770d7e1e4f04d204d8652
Dhanesh Shetty <dhaneshshetty65@gmail.com>
2022-05-19 16:52:07 +0530

    Feat:result page UI

--- app/src/main/java/com/ddevs/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/ddevs/bmicalculator/MainActivity.kt
@@ -7,53 +7,70 @@ import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextAlign.Companion.Center
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.text.isDigitsOnly
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.ddevs.bmicalculator.ui.theme.BMICalculatorTheme
-import com.ddevs.bmicalculator.ui.theme.Purple200
-import com.ddevs.bmicalculator.ui.theme.Purple500
+import com.ddevs.bmicalculator.ui.theme.backgroundPurple
+import com.ddevs.bmicalculator.ui.theme.lightpurple
+import com.ddevs.bmicalculator.ui.theme.red
+
 
 class MainActivity : ComponentActivity() {
-    private val viewModel:MainViewModel by lazy{
+    private val viewModel: MainViewModel by lazy {
         ViewModelProvider(this).get(MainViewModel::class.java)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BMICalculatorTheme {
-                val navController = rememberNavController()
-                NavHost(navController, startDestination = "main") {
-                    composable("main") { MainPage(navController,viewModel) }
-                    composable("result") { ResultScreen(viewModel) }
+                Surface(color = backgroundPurple) {
+                    val navController = rememberNavController()
+                    NavHost(navController, startDestination = "main") {
+                        composable("main") { MainPage(navController, viewModel) }
+                        composable("result") { ResultScreen(viewModel) }
+                    }
                 }
             }
         }
+
     }
 }
 
 @Composable
 fun MainPage(navController: NavController,viewModel:MainViewModel) {
-    Column(modifier = Modifier
-        .padding(8.dp)
-        .fillMaxSize()) {
+    Column(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()), verticalArrangement = Arrangement.SpaceEvenly
+    ) {
         Text(
-            "Hello Dear User",
+            "Enter the details",
             modifier = Modifier.padding(5.dp),
-            style = MaterialTheme.typography.h5
+            style = MaterialTheme.typography.h5,
+            color = Color.White
         )
         Row(
             modifier = Modifier
@@ -85,7 +102,7 @@ fun MainPage(navController: NavController,viewModel:MainViewModel) {
                     .weight(1f)
                     .padding(vertical = 4.dp),
                 text = "Weight",
-                onChange = { if(it.isNotEmpty()){viewModel.weight = Integer.parseInt(it)} },
+                onChange = { if(it.isDigitsOnly() || it.isBlank()) viewModel.weight = it},
                 value = viewModel.weight,
                 unit = "kg"
             )
@@ -94,7 +111,7 @@ fun MainPage(navController: NavController,viewModel:MainViewModel) {
                     .weight(1f)
                     .padding(vertical = 4.dp),
                 text = "Height",
-                onChange = { if(it.isNotEmpty()){viewModel.height = Integer.parseInt(it) }},
+                onChange = {  if(it.isDigitsOnly() || it.isBlank()) viewModel.height = it},
                 value = viewModel.height,
                 unit = "cm"
             )
@@ -104,17 +121,17 @@ fun MainPage(navController: NavController,viewModel:MainViewModel) {
                 .fillMaxWidth()
                 .padding(vertical = 4.dp),
             text = "Age",
-            onChange = { if(it.isNotEmpty()){viewModel.age = Integer.parseInt(it)}},
+            onChange = { if(it.isDigitsOnly() || it.isBlank()) viewModel.age = it },
             value = viewModel.age,
             unit = "years"
         )
         Button(
-            { calculateBMI(navController,viewModel)},
+            { calculateBMI(navController, viewModel) },
             Modifier
                 .fillMaxWidth()
                 .padding(4.dp),
-            shape = MaterialTheme.shapes.large,
-            colors = ButtonDefaults.buttonColors(backgroundColor = Purple500),
+            shape = RoundedCornerShape(percent = 20),
+            colors = ButtonDefaults.buttonColors(backgroundColor = red, contentColor = Color.White),
         ) {
             Text("Calculate BMI",Modifier.padding(8.dp))
         }
@@ -133,7 +150,10 @@ fun RadioButton(
     onClick: () -> Unit
 ) {
     val surfaceColor by animateColorAsState(
-        if (isEnabled) Purple500 else Purple200,
+        if (isEnabled) red else lightpurple,
+    )
+    val textColor by animateColorAsState(
+        if(isEnabled) Color.White else Color.Black
     )
     val elevation by animateDpAsState(targetValue = if (isEnabled) 2.dp else 1.dp)
     Surface(
@@ -155,8 +175,9 @@ fun RadioButton(
                 Text(
                     text,
                     Modifier.padding(4.dp),
-                    textAlign = TextAlign.Center,
-                    style = MaterialTheme.typography.h6
+                    textAlign = Center,
+                    style = MaterialTheme.typography.h6,
+                    color = textColor
                 )
             }
     }
@@ -167,7 +188,7 @@ fun InputField(
     modifier: Modifier,
     text: String,
     onChange: (String) -> Unit,
-    value: Int,
+    value: String,
     unit: String
 ) {
     Column(
@@ -175,27 +196,124 @@ fun InputField(
         verticalArrangement = Arrangement.SpaceEvenly,
         horizontalAlignment = Alignment.Start
     ) {
-        Text(modifier = Modifier.padding(vertical = 4.dp), text = text)
-        OutlinedTextField(
-            value.toString(), onChange, label = { Text("$text($unit)") },
+        Text(modifier = Modifier.padding(vertical = 4.dp), text = text, color = Color.LightGray)
+        TextField(
+            value, onChange, label = { Text("$text($unit)") },
             keyboardOptions = KeyboardOptions(
                 keyboardType = KeyboardType.Number,
                 imeAction = ImeAction.Next
+            ), modifier = Modifier
+                .padding(vertical = 8.dp, horizontal = 4.dp)
+                .fillMaxWidth(),
+            textStyle= LocalTextStyle.current.copy(fontSize=24.sp),
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                backgroundColor = lightpurple,
+                textColor = red,
+                unfocusedLabelColor = Color.White,
+                focusedLabelColor = red,
+                unfocusedBorderColor = Color.LightGray
             ),
+            isError = value.isBlank(),
         )
+        if(value.isBlank()){
+            Text("Input should not be blank",color=red,style=MaterialTheme.typography.caption)
+        }
     }
 }
 
 fun calculateBMI(navController: NavController,viewModel: MainViewModel){
-    viewModel.calculateBMI()
-    navController.navigate("result"){
-        popUpTo("main")
+    if(viewModel.validate()) {
+        navController.navigate("result") {
+            popUpTo("main")
+        }
     }
 }
-
+fun Float.format(digits: Int) = "%.${digits}f".format(this)
 @Composable
 fun ResultScreen(viewModel: MainViewModel) {
-    Text("BMI:"+viewModel.state.name+"Value:"+viewModel.bmi)
+    val colorText by animateColorAsState(
+        when (viewModel.state) {
+            MainViewModel.BmiCategories.Normal -> Color.Green
+            MainViewModel.BmiCategories.Overweight -> Color.Blue
+            MainViewModel.BmiCategories.Underweight -> Color.Yellow
+        }
+    )
+
+    val statement = (when (viewModel.state) {
+        MainViewModel.BmiCategories.Normal -> "You have a normal body weight.Good job!"
+        MainViewModel.BmiCategories.Overweight -> "You have a overweight body.You can do this,exercise more"
+        MainViewModel.BmiCategories.Underweight -> "You need to increase your weight.You are underweight."
+    })
+
+    Column(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            "Your Result",
+            style = MaterialTheme.typography.h5,
+            modifier = Modifier.align(Alignment.Start),
+            color = Color.White
+        )
+        Image(
+            painterResource(R.drawable.ic_undraw_fitness_stats), "Stats image", modifier = Modifier
+                .size(200.dp)
+                .padding(4.dp)
+        )
+        Surface(
+            shape = MaterialTheme.shapes.small, color = red, modifier = Modifier
+                .align(
+                    Alignment.CenterHorizontally
+                )
+                .padding(vertical = 8.dp)
+                .fillMaxWidth(),
+            elevation = (8).dp
+        ) {
+            Column(
+                verticalArrangement = Arrangement.SpaceEvenly,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    viewModel.state.name,
+                    color = colorText,
+                    style = MaterialTheme.typography.h6,
+                    modifier = Modifier.padding(vertical = 4.dp)
+                )
+                Text(
+                    viewModel.bmi.format(2),
+                    color = Color.White,
+                    style = LocalTextStyle.current.copy(fontSize=48.sp),
+                    modifier = Modifier.padding(vertical = 4.dp)
+                )
+                Text(
+                    "Height:${viewModel.height} cm",
+                    color = Color.LightGray,
+                    modifier = Modifier.padding(4.dp)
+                )
+                Text(
+                    "Weight:${viewModel.weight} kg",
+                    color = Color.LightGray,
+                    modifier = Modifier.padding(4.dp)
+                )
+                Text(
+                    "Age:${viewModel.age} years",
+                    color = Color.LightGray,
+                    modifier = Modifier.padding(4.dp)
+                )
+                Text(
+                    statement,
+                    color = Color.White,
+                    modifier = Modifier.padding(8.dp),
+                    style = MaterialTheme.typography.h6,
+                    textAlign = Center
+                )
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/ddevs/bmicalculator/MainViewModel.kt
+++ app/src/main/java/com/ddevs/bmicalculator/MainViewModel.kt
@@ -7,21 +7,42 @@ import androidx.lifecycle.ViewModel
 import kotlin.math.pow
 
 class MainViewModel: ViewModel() {
-    var weight by mutableStateOf(60)
-    var height by mutableStateOf(160)
-    var age by mutableStateOf(20)
+    var weight by mutableStateOf("60")
+    var height by mutableStateOf("160")
+    var age by mutableStateOf("20")
     var gender by mutableStateOf("Male")
 
     var state by mutableStateOf(BmiCategories.Normal)
     var bmi by mutableStateOf(0f)
+    var error: String? by mutableStateOf(null)
 
     enum class BmiCategories{
         Underweight,Normal,Overweight
     }
 
-    fun calculateBMI(){
-        val heightsq:Float=((height.toFloat()/100))
-        bmi= (weight.toFloat()/ heightsq.toDouble().pow(2.0)).toFloat()
+    fun validate():Boolean{
+        val heightCal: Int
+        val weightCal: Int
+        if(height.isBlank()){
+            error="Fill a value for height"
+            return false
+        }
+        else
+            heightCal=Integer.parseInt(height)
+        if(weight.isBlank()){
+            error="Fill a value for weight"
+            return false
+        }
+        else
+            weightCal=Integer.parseInt(weight)
+        calculateBMI(heightCal, weightCal)
+        error=null
+        return true
+    }
+
+    private fun calculateBMI(heightCal:Int, weightCal:Int){
+        val heightsq:Float=((heightCal.toFloat()/100))
+        bmi= (weightCal.toFloat()/ heightsq.toDouble().pow(2.0)).toFloat()
         state = when {
             bmi<18.5 -> BmiCategories.Underweight
             bmi in 18.5..25.0 -> BmiCategories.Normal
